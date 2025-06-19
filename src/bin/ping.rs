276b4fe9665dd218c0c00fe1519636f18c4610d9
rllola <rllola80@gmail.com>
2025-06-19 19:15:42 +0200

    fix ping

--- src/bin/ping.rs
+++ src/bin/ping.rs
@@ -24,16 +24,16 @@ fn main() {
     let timeout_duration = Duration::from_millis(3000);
 
     for row in client.query("SELECT * FROM nodes WHERE last_ping_timestamp IS NULL OR (last_ping_timestamp < NOW() - INTERVAL '5 minutes')", &[]).unwrap() {
-        let address: String = row.get(0);
+        let ip: String = row.get(0);
         let tcp_port: i32 = row.get(1);
         // let _udp_port: i32 = row.get(2);
         // let _node_id: Vec<u8> = row.get(3);
 
-        let socket_address = format!("{}:{}", address, tcp_port).parse().unwrap();
+        let socket_address = format!("{}:{}", ip, tcp_port).parse().unwrap();
 
         match TcpStream::connect_timeout(&socket_address, timeout_duration) {
             Ok(_) => {
-                info!("{} on port {} is working", address, tcp_port);
+                info!("{} on port {} is working", ip, tcp_port);
                 if let Err(err) = client.execute(
                     "UPDATE nodes SET last_ping_timestamp = NOW() WHERE ip = $1 AND tcp_port = $2",
                     &[&ip, &tcp_port],
